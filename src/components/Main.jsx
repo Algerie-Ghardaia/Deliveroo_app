9828e3b46be789357ce85f111f578972071fade1
Algerie-Ghardaia <160014953+Algerie-Ghardaia@users.noreply.github.com>
2024-03-27 20:19:02 +0100

    added cart

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,80 @@
 import React, { useState } from "react";
+import "../App.css";
 //--------REACT LIBRARY---------//
 import { AiFillStar } from "react-icons/ai";
 import { CiCirclePlus } from "react-icons/ci";
 import { CiCircleMinus } from "react-icons/ci";
 //-----------------------------//
 
-export default function Main({ data, setData }) {
+export default function Main({ data }) {
   const [article, setArticle] = useState({});
-  const handlSelectArticle = (meal) => {
-    console.log(meal.id);
+  const [cart, setCart] = useState([]);
+
+  const handlAddSelectArticle = (meal) => {
     const article = { ...meal };
     setArticle(article);
-    // console.log(meal.title+'\n'+ meal.price);
-    // console.log(article.title);
+    const newCart = [...cart];
+    const found = newCart.find((elem) => elem.id === meal.id);
+    if (found) {
+      found.quantity++;
+    } else {
+      newCart.push({ ...article, quantity: 1 });
+    }
+    setCart(newCart);
   };
-  console.log("Mon article " + article);
+
+  const handlDeleteSelectArticle = (t) => {
+    const newCart = [...cart];
+
+    const found = newCart.find((elem) => elem.id === t.id);
+
+    if (found.quantity === 1) {
+      const index = newCart.indexOf(found);
+      newCart.splice(index, 1);
+    } else {
+      found.quantity--;
+    }
+
+    setCart(newCart);
+  };
+
+  let total = 0;
+  let livrasion = Number(3,66)
+  console.log(livrasion);
+  for (let i = 0; i < cart.length; i++) {
+    const meal = cart[i];
+    total = total + meal.price * meal.quantity;
+  }
+
+  const listCommande = cart.map((t) => {
+    console.log(t.id);
+    return (
+      <>
+        <div key={t.id} className="click_client">
+          <div className="icon_2_number">
+            <CiCirclePlus
+              color="#07a497"
+              size={25}
+              onClick={() => {
+                handlAddSelectArticle(t);
+              }}
+            />
+            <div className="number">{t.quantity}</div>
+            <CiCircleMinus
+              color="#07a497"
+              size={25}
+              onClick={() => {
+                handlDeleteSelectArticle(t);
+              }}
+            />
+          </div>
+
+          <div className="title_selection">{t.title}</div>
+          <div className="price_selection">{(t.price)*(t.quantity)}€</div>
+        </div>
+      </>
+    );
+  });
   return (
     <>
       <main>
@@ -32,7 +92,7 @@ export default function Main({ data, setData }) {
                             key={meal.id}
                             className="glob"
                             onClick={() => {
-                              handlSelectArticle(meal);
+                              handlAddSelectArticle(meal);
                             }}
                           >
                             <div>
@@ -52,7 +112,6 @@ export default function Main({ data, setData }) {
                                 </span>
                               )}
                             </div>
-                            {/* className={(meal.description ) ? "":"sans_p"} */}
                             <div>
                               {meal.picture && (
                                 <img src={meal.picture} alt={meal.title} />
@@ -71,39 +130,40 @@ export default function Main({ data, setData }) {
           </section>
           <section className="col-right">
             <div className="panier">
-              <div className="btn_p">
-                <button
-                  className={article.title ? "button isButton" : "button"}
-                >
-                  Valide mon panier
-                </button>
-                {/* {article.title ? "":"<p>Votre panier est vide</p>"} */}
-                <div className="click_client"> 
-                  <div className="icon_2_number">
-                    <CiCirclePlus color="#07a497" size={25} />
-                    <div className="number">1</div>
-                    <CiCircleMinus color="#07a497" size={25} />
+              {/* PANIR VIDE */}
+              <div className={article.title ? "btn_p display" : "btn_p"}>
+                <button className="button">Valide mon panier</button>
+                <div className="vide">Votre panier et vide</div>
+              </div>
+              {/*-------------*/}
+              {/* PANIER AVEC COMMANDE */}
+              <div className={!article.title ? "display" : "btn_p"}>
+                <div>
+                  <button
+                    className={article.title ? "button isButton" : "button"}
+                  >
+                    Valide mon panier
+                  </button>
+                </div>
+                <div>{listCommande}</div>
+                <hr className="ligne" />
+                <div className="calc">
+                  <div className="Sous-total">
+                    <h6>Sous-total</h6>
+                    <h6>{total}€</h6>
                   </div>
-                  <div className="title_selection">{article.title}</div>
-                  <div className="price_selection">{article.price} €</div>
+                  <div className="Sous-total">
+                    <h6>Frais de livraison</h6>
+                    <h6>{livrasion}</h6>
+                  </div>
                 </div>
-              </div>
-              <hr className="ligne" />
-              <div className="calc">
+                <hr className="ligne" />
                 <div className="Sous-total">
-                  <h6>Sous-total</h6>
-                  <h6>{article.price} €</h6>
-                </div>
-                <div className="Sous-total">
-                  <h6>Frais de livraison</h6>
-                  <h6>3,30 €</h6>
+                  <div className="total">Total</div>
+                  <div className="prix_total">{Number(total+livrasion).toFixed(2)}</div>
                 </div>
               </div>
-              <hr className="ligne" />
-              <div className="Sous-total">
-                <div className="total">Total</div>
-                <div className="prix_total">28,30 €</div>
-              </div>
+              {/*----------------------*/}
             </div>
           </section>
         </div>
---
+++ src/components/Panier.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "../App.css";
+
+
+export default function Panier() {
+  
+  return (
+    <>
+      <div className="btn_p">
+        <button className="button">Valide mon panier</button>
+        <div className="vide">Votre panier et vide</div>
+      </div>
+    </>
+  );
+}
